7d63009f01c375dee814312d1e826f97545a28b4
Сергей Самовол <s.samovol@outlook.com>
2015-12-15 23:09:51 +0300

    lal

--- ConsoleApplication4/Source.cpp
+++ ConsoleApplication4/Source.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 void main()
 {
-	signed int m1[5], m2[5];
+	signed int m1[5], m2[5], temp;
 	cout << "Input 1st mass" << endl;
 	for (int i = 0;i < 5; i++)
 	{
@@ -51,17 +51,70 @@ void main()
 			}
 		}
 	}
-	/*
-	cout << endl;
+	cout << "First mass sorted:" << endl;
 	for (int i = 0;i < 5; i++)
 	{
 	cout << m1[i]<<" ";
 	}
 	cout << endl;
+	cout << "Second mass sorted:" << endl;
 	for (int i = 0;i < 5; i++)
 	{
 	cout << m2[i]<<" ";
 	}
 	cout << endl;
-	*/
+	
+	for (int i = 0; i < 5; i++)
+	{
+		if (m1[i] > 0)
+			for (int j = 0; j < 5; j++)
+			{
+				if (m2[j] < 0)
+				{
+					temp = m1[i];
+					m1[i] = m2[j];
+					m2[j] = temp;
+					break;
+				}
+				if (m2[4] > 0)
+				{
+					m1[i] = 0;
+				}
+			}
+
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (m2[i] < 0)
+			for (int j = 0; j < 5; j++)
+			{
+				if (m1[j] > 0)
+				{
+					temp = m2[i];
+					m2[i] = m1[j];
+					m1[j] = temp;
+					break;
+				}
+				if (m1[4] < 0)
+				{
+					m2[i] = 0;
+				}
+			}
+
+	}
+
+	cout << "First mass replaced:" << endl;
+	for (int i = 0;i < 5; i++)
+	{
+		cout << m1[i] << " ";
+	}
+	cout << endl;
+	cout << "Second mass replaced:" << endl;
+	for (int i = 0;i < 5; i++)
+	{
+		cout << m2[i] << " ";
+	}
+	cout << endl;
+
 }
